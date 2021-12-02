55d52af2cd2bfe1c2b900719905c737a9deeb896
anastaciem <91271992+anastaciem@users.noreply.github.com>
2021-12-02 19:29:22 +0200

    Add files via upload

---
+++ lab6_1item/Source.cpp
@@ -0,0 +1,67 @@
+// lab6_1iter.cpp
+// < Демчук Анастасія >
+// Лабораторна робота № 6.1
+//  Пошук елементів одновимірного масиву ітераційним та рекурсивним способом //
+// Варіант 14
+#include <iostream>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+
+void Create(int* a, const int size, const int Low, const int High)
+{
+    for (int i = 0; i < size; i++)
+        a[i] = Low + rand() % (High - Low + 1);
+}
+void Print(int* a, const int size)
+{
+    for (int i = 0; i < size; i++)
+        cout << setw(4) << a[i];
+    cout << endl;
+}
+int Counting(int* a, const int size)
+{
+    int count = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (abs(a[i]) % 2 != 0 && abs(i % 13 != 0))
+        {
+            count++;
+        }
+    }
+    return count;
+}
+void NewMas(int* a, const int size)
+{
+    for (int i = 0; i < size; i++)
+        if (abs(a[i]) % 2 != 0 && abs(i % 13 != 0))
+            a[i] = 0;
+}
+int Sum(int* a, const int size)
+{
+    int S = 0;
+    for (int i = 0; i < size; i++)
+        if (abs(a[i]) % 2 != 0 && abs(i % 13 != 0))
+            S += a[i];
+    return S;
+}
+int main()
+{
+    srand((unsigned)time(NULL));
+    const int n = 25;
+    int a[n];
+
+    int Low = 15;
+    int High = 94;
+
+    Create(a, n, Low, High);
+    Print(a, n);
+
+    cout << "Sum = " << Sum(a, n) << endl;
+    cout << "Count = " << Counting(a, n) << endl;
+
+    NewMas(a, n);
+    Print(a, n);
+
+    return 0;
+}
---
+++ unit6_1item/unit6_1item.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace unit61item
+{
+	TEST_CLASS(unit61item)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int i = 2;
+			int p = i % 2;
+			Assert::AreEqual(p, 0);
+		}
+	};
+}
